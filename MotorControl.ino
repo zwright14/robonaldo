38b47c4c5f9653a0770f69e494c0814916f004c0
Zach Wright <zwright14@gmail.com>
2018-02-04 15:20:14 -0600

    Add files via upload
    Added additional support to control the direction of rotation for a motor

--- MotorControl.ino
+++ MotorControl.ino
@@ -1,20 +1,70 @@
-const int switchPin = 2;
-const int motorPin = 9;
-int switchState = 0;
-  
+// Define pins that need to be used
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+const int enablePin = 9;
+const int directionSwitchPin = 4;
+const int onOffSwitchPin = 5;
+const int potPin = A0;
+
+// Define states
+int onOffState = 0;
+int previousOnOffState = 0;
+int directionState = 0;
+int previousDirectionState = 0;
+
+// Define motor speecifications
+int motorEnabled = 0;
+int motorSpeed = 0;
+int motorDirection = 0;
+
+
 void setup() {
-  pinMode(motorPin, OUTPUT);
-  pinMode(switchPin, INPUT);
+  pinMode(directionSwitchPin, INPUT);
+  pinMode(onOffSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, LOW);
 }
 
 void loop() {
-  switchState = digitalRead(switchPin);
+  onOffState = digitalRead(onOffSwitchPin);
+  delay(1);
+  directionState = digitalRead(directionSwitchPin);
+  motorSpeed = analogRead(potPin)/4;
   
-  if(switchState == HIGH) {
-    digitalWrite(motorPin, HIGH);
+  //Enable or disable the motor when pressing onOffSwitchPin
+  if(onOffState != previousOnOffState) {
+    if(onOffState == HIGH) {
+      motorEnabled = !motorEnabled;
+    }
+  }
+  
+  //Switch the direction of the motor when pressing directionSwitchPin
+  if (directionState != previousDirectionState) {
+    if (directionState == HIGH) {
+      motorDirection = !motorDirection;
+    }
+  }
+  
+  if (motorDirection == 1) {
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
   }
   
   else {
-    digitalWrite(motorPin, LOW);
+    digitalWrite(controlPin2, HIGH);
+    digitalWrite(controlPin1, LOW);
   }
+  
+  if (motorEnabled == 1) {
+    analogWrite(enablePin, motorSpeed);
+  }
+  
+  else {
+    analogWrite(enablePin, 0);
+  }
+  
+  previousDirectionState = directionState;
+  previousOnOffState = onOffState; 
 }
